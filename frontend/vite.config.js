0f2831ec3bb6e2f35fea22029f01db21abe3691d
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2026-02-14 04:32:18 +0600

    fixes frontend imports

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import path from "path"
 import { fileURLToPath } from "url"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import process from "process"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
